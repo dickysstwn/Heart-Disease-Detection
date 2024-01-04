e1be62eafe3aa35e1d35cb5c2b03aed89015e968
Dicky Setiawan <118114842+dickysstwn@users.noreply.github.com>
2024-01-04 12:40:52 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -86,7 +86,7 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/random_forest_model.pkl", 'rb'))
+model = pickle.load(open("model/xgb_model.pkl", 'rb'))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
